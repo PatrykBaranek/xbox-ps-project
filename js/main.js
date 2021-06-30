5e528796b3e96fd4f934de01947f9d220594b211
Patryk <cien2460045@gmail.com>
2021-06-30 16:43:10 +0200

    Added pop-up text and hiding animation for pop-up

--- js/main.js
+++ js/main.js
@@ -4,8 +4,10 @@ const psBtn = document.querySelector(".ps-btn");
 xbxBtn.addEventListener("click", () => {
 	psBtn.classList.add("hide");
 	xbxBtn.classList.add("from-left-to-right");
+	xbxBtn.classList.add("to-main-page-animation");
 });
 psBtn.addEventListener("click", () => {
 	xbxBtn.classList.add("hide");
 	psBtn.classList.add("from-right-to-left");
+	psBtn.classList.add("to-main-page-animation");
 });
