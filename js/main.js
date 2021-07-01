3c9db126ab9c38e8575a190ae614a9276b224066
Patryk <cien2460045@gmail.com>
2021-07-01 17:51:04 +0200

    Added Nav and animation to nav

--- js/main.js
+++ js/main.js
@@ -1,6 +1,36 @@
 const xbxBtn = document.querySelector(".xbx-btn");
 const psBtn = document.querySelector(".ps-btn");
 
+const navBtn = document.querySelector(".burger-btn");
+const nav = document.querySelector(".nav");
+const allNavItems = document.querySelectorAll(".nav__item");
+
+// Nav animation handler
+const handleNav = () => {
+	nav.classList.toggle("nav--active");
+
+	allNavItems.forEach((item) => {
+		item.addEventListener("click", () => {
+			nav.classList.remove("nav--active");
+		});
+	});
+
+	handleNavItemsAnimation();
+};
+
+const handleNavItemsAnimation = () => {
+	let delayTime = 0;
+
+	allNavItems.forEach((item) => {
+		item.classList.toggle("nav-items-animation");
+		item.style.animationDelay = "." + delayTime + "s";
+		delayTime++;
+	});
+};
+
+navBtn.addEventListener("click", handleNav);
+
+// Start Page
 xbxBtn.addEventListener("click", () => {
 	psBtn.classList.add("hide");
 	xbxBtn.classList.add("from-left-to-right");
